458cde32976f5ff9a7964625c9b5cb668be94601
mohameml <mhmdahmdnbyl18@gmail.com>
2025-03-04 13:51:24 +0100

    project  usePopCorn init

--- Projets/07_usepopcorn/src/main.jsx
+++ Projets/07_usepopcorn/src/main.jsx
@@ -1,47 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
-
-import StarRating from "./components/StarRating/index.jsx";
-
-// export default function Test() {
-// 	const [movieRating, setMovieRating] = useState(0);
-
-// 	return (
-// 		<div>
-// 			<StarRatingr
-// 				color="blue"
-// 				maxRating={10}
-// 				onSetRating={setMovieRating}
-// 			/>
-// 			<p>This movie whas rating {movieRating} stars</p>
-// 		</div>
-// 	);
-// }
+import "./index.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<StarRating
-			maxRating={"sssisis"}
-			// messages={["Terrible", "Bad", "OK", "Good", "Amazing"]}
-		/>
-
-		{/* <StarRatingr maxRating={10} size={24} color="red" defaultRating={3} /> */}
-
-		{/* <Test /> */}
-		{/* ================ Error ============ */}
-		{/* <StarRatingr
-			maxRating={5}
-			messages={["Terrible", "OK", "Good", "Amazing"]}
-		/> */}
-
-		{/* =========== Error ============ */}
-		{/* <StarRatingr maxRating={10} size={24} color="red" defaultRating={"3"} /> */}
-
-		{/* =========== Error ============== */}
-		{/* <StarRatingr maxRating={10} size={24} color="red" defaultRating={11} /> */}
-
-		{/* <App /> */}
+		<App />
 	</StrictMode>
 );
